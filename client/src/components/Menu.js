5c9db32bc028fcede8fcb6033e364f5388b3c0f7
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-06 06:31:44 -0400

    feat: login button

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -55,6 +55,22 @@ const Hr = styled.hr`
   border: 0.5px solid #373737;
 `;
 
+const Login = styled.div``;
+
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
 const Menu = () => {
   return (
     <Container>
@@ -84,6 +100,14 @@ const Menu = () => {
           <HistoryOutlinedIcon />
           History
         </Item>
+        <Hr />
+        <Login>
+          Sign in to like videos, comments and subscribe.
+          <Button>
+            <AccountCircleOutlinedIcon />
+            SIGN IN
+          </Button>
+        </Login>
         <Hr />
         <Item>
           <LibraryMusicOutlinedIcon />
