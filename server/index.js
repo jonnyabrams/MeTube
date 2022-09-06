80350d7270cfa730e98622ae60b2a5337ab13677
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-06 10:51:20 -0400

    feat: allow app to use json

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8800;
 
+app.use(express.json());
+
 app.use("/api/videos", videoRoutes);
 app.use("/api/comments", commentRoutes);
 
