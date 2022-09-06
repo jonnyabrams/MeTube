317ad087d942ada9700b6e992cb296ce971b06ad
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-06 15:55:02 -0400

    feat: Menu home icon links to home

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -95,10 +95,12 @@ const Menu = ({ darkMode, setDarkMode }) => {
             YourTube
           </Logo>
         </Link>
-        <Item>
-          <HomeIcon />
-          Home
-        </Item>
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <Item>
+            <HomeIcon />
+            Home
+          </Item>
+        </Link>
         <Item>
           <ExploreOutlinedIcon />
           Explore
