4501c52de68df907de304c23f7e5eac0fba2c381
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-06 06:43:33 -0400

    feat: enable dark mode

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -20,9 +20,9 @@ import YouTubeLogo from "../img/youtube-logo.png";
 
 const Container = styled.div`
   flex: 1;
-  background-color: #202020;
+  background-color: ${({ theme }) => theme.bg};
   height: 100vh;
-  color: white;
+  color: ${({ theme }) => theme.text};
   font-size: 14px;
   position: sticky;
   top: 0;
@@ -54,7 +54,7 @@ const Item = styled.div`
 
 const Hr = styled.hr`
   margin: 15px 0px;
-  border: 0.5px solid #373737;
+  border: 0.5px solid ${({ theme }) => theme.soft};
 `;
 
 const Login = styled.div``;
@@ -73,7 +73,7 @@ const Button = styled.button`
   gap: 5px;
 `;
 
-const Menu = () => {
+const Menu = ({ darkMode, setDarkMode }) => {
   return (
     <Container>
       <Wrapper>
@@ -148,7 +148,7 @@ const Menu = () => {
           <HelpOutlineOutlinedIcon />
           Help
         </Item>
-        <Item>
+        <Item onClick={() => setDarkMode(!darkMode)}>
           <SettingsBrightnessOutlinedIcon />
           Light Mode
         </Item>
