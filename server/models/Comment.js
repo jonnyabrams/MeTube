100b6d6a1cf268999165c2f4f26ed0f6b7d69be2
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-06 10:47:53 -0400

    feat: create models

---
+++ server/models/Comment.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const CommentSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    videoId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Comment", CommentSchema);
