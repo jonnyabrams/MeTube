56b0f0cf79e3c9eeb7fa38d0bf7e8f68cca13ca1
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-06 06:05:35 -0400

    chore: structure app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
-const App = () => {
-  return <div className="App">
-    
-  </div>;
-};
+import styled from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
 
-export default App;
+const Container = styled.div``;
+
+const Main = styled.div``;
+
+const Wrapper = styled.div``;
+
+const App = () => {
+  return (
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>video cards</Wrapper>
+      </Main>
+    </Container>
+  );
+};
