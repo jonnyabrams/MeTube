b7ad601cd6e773c89d00f30d4043e55271529d49
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-06 09:25:13 -0400

    feat: design Video page

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -86,7 +86,7 @@ const Menu = ({ darkMode, setDarkMode }) => {
   return (
     <Container>
       <Wrapper>
-        <Link to="/" style={{ textDecoration: "none" }}>
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
           <Logo>
             <Img src={YouTubeLogo} />
             YourTube
