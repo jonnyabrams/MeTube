e3ed7b044d3ac6fda16a3cbe54f7da0c4d46f5a9
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-06 11:01:43 -0400

    feat: video routes & controllers

---
+++ server/controllers/video.js
@@ -0,0 +1,51 @@
+import { createError } from "../error.js";
+
+import Video from "../models/Video.js";
+
+export const addVideo = async (req, res, next) => {
+  const newVideo = new Video({ ...req.body });
+  try {
+    const savedVideo = await newVideo.save();
+    res.status(200).send(savedVideo);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getVideo = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    res.status(200).send(video);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const addView = async (req, res, next) => {
+  try {
+    await Video.findByIdAndUpdate(req.params.id, {
+      $inc: { views: 1 },
+    });
+    res.status(200).send("View count increased");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const random = async (req, res, next) => {
+  try {
+    const videos = await Video.aggregate([{ $sample: { size: 40 } }]);
+    res.status(200).send(videos);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const trend = async (req, res, next) => {
+  try {
+    const videos = await Video.find().sort({ views: -1 }); // 1 for least viewed, -1 for most viewed
+    res.status(200).send(videos);
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/videoRoutes.js
+++ server/routes/videoRoutes.js
@@ -1,5 +1,19 @@
 import express from "express";
 
+import {
+  addVideo,
+  addView,
+  getVideo,
+  random,
+  trend,
+} from "../controllers/video.js";
+
 const router = express.Router();
+
+router.post("/", addVideo);
+router.get("/find/:id", getVideo);
+router.put("/view/:id", addView);
+router.get("/trend", trend);
+router.get("/random", random);
 
 export default router;
